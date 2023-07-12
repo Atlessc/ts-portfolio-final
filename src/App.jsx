f33c2b779bafd9a59a9b45764fdd067ce8cd492e
Tyler Smith <tylersmith4596@gmail.com>
2023-07-11 21:26:20 -0700

    okay i fixed the CSS but im getting rid of the GA

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,8 @@ import './App.css'
 import { FooterSection } from './components/footer'
 import { HomePage } from './components/home'
 import { NavSection } from './components/nav'
-import ReactGA from 'react-ga'
-import { useEffect } from 'react'
-
-const TRACKING_ID ="UA-277352337-1";
-ReactGA.initialize(TRACKING_ID);
-
 
 function App() {
-
-  useEffect(() => {
-    // non interactions
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, [])
 
   return (
       <div className='App'>
